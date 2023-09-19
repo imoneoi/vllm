ea95709a85ad9629ceb8c2df63f6f53c92f313f6
One <imone@tuta.io>
2023-09-19 06:18:34 +0000

    fix formatting

--- vllm/model_executor/layers/attention.py
+++ vllm/model_executor/layers/attention.py
@@ -264,8 +264,9 @@ class PagedAttentionWithRoPE(PagedAttention):
         self.is_neox_style = is_neox_style
 
         # Create the cos and sin cache.
-        inv_freq = 1.0 / (base**(
-            torch.arange(0, rotary_dim, 2, dtype=torch.float32, device="cuda") / rotary_dim))
+        inv_freq = 1.0 / (base**(torch.arange(
+            0, rotary_dim, 2, dtype=torch.float32, device="cuda") /
+                                 rotary_dim))
         t = torch.arange(max_position, dtype=torch.float32, device="cuda")
         freqs = torch.einsum("i,j -> ij", t, inv_freq)
         cos = freqs.cos()
